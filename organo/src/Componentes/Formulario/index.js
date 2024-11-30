f8638d4e9730f764e49a3c4a3e955c577fd6e95c
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-11-30 13:09:38 -0300

    Adicionando lista e botão ao formulário.

--- organo/src/Componentes/Formulario/index.js
+++ organo/src/Componentes/Formulario/index.js
@@ -1,7 +1,20 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 export const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        ' Inovação e Gestão'
+    ]
+
     return (
         <section className='Formulario'>
             <form >
@@ -9,6 +22,8 @@ export const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome." />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} />
+                <Botao>Criar card</Botao>
             </form>
         </section>
     )
