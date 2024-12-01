96a48d14005b57fe2b3949313eb15dbb3b1ece96
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-12-01 11:31:21 -0300

    Exibindo somente times que possuem pelo menos um integrante na equipe.

--- organo/src/Componentes/Time/index.js
+++ organo/src/Componentes/Time/index.js
@@ -3,6 +3,7 @@ import './Time.css';
 
 const Time = (props) => {
     return (
+        props.colaboradores.length > 0 &&
         <section className='Time' style={{ backgroundColor: props.corPrimaria }}>
             <h3 style={{ borderColor: props.corSecondaria }}>{props.nome}</h3>
             <div className='Colaboradores'>
