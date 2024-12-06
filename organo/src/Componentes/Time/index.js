240f69e5579d0dda035e98a2138ac5653d2e1294
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-12-06 10:46:05 -0300

    Adicionando id ao time.

--- organo/src/Componentes/Time/index.js
+++ organo/src/Componentes/Time/index.js
@@ -6,7 +6,7 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
         colaboradores.length > 0 &&
         <section className='Time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
-                <input onChange={event => mudarCor(event.target.value, time.nome)} value={time.cor} type='color' className='Cor-Time'></input>
+                <input onChange={event => mudarCor(event.target.value, time.id)} value={time.cor} type='color' className='Cor-Time'></input>
                 <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
                 <div className='Colaboradores'>
                     {colaboradores.map(
